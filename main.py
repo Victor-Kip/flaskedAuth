a84788a30b0000cb34a09ca11910418117c24a84
Victor Kip <vicklan132@gmail.com>
2025-08-28 09:03:03 +0300

    add main flask file and endpoints

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template,request,redirect,session,url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = 'mon_secret_key'
+
+#configure SQLAlchemy
+app.config("SQLALCHEMY_DATABASE_URI") ="sqlite:///users.db"
+
+#database model
+
+ #routes
+@app.route("/")
+def home():
+    if "username" in session:
+        return redirect(url_for('dashboard'))
+    return render_template('index.html')
+
+
+if __name__ in "__main__":
+    app.run(debug=True)
